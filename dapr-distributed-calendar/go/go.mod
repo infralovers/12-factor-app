4cc31a690416f1cf24eed4d44ba4e6f2e6de34f6
Martin Buchleitner <martin@nitram.at>
2024-01-10 09:04:22 +0100

    refactoring setup (#2)
    * refactoring setup
    
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>
    
    * container matrix build
    
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>
    
    ---------
    
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- dapr-distributed-calendar/go/go.mod
+++ dapr-distributed-calendar/go/go.mod
@@ -1,5 +1,5 @@
-module go
+module go_events
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- dapr-distributed-calendar/python/messages.py
+++ dapr-distributed-calendar/python/messages.py
@@ -1,14 +1,16 @@
+import json
+import os
+
 import flask
 from flask import request, jsonify
 from flask_cors import CORS
-import json
-import sys
-import time
 
 from dapr.clients import DaprClient
 
 app = flask.Flask(__name__)
 CORS(app)
+
+dapr_port = os.getenv("DAPR_HTTP_PORT", 5000)
 
 # dapr calls this endpoint to register the subscriber configuration
 # an alternative way would to be declare this inside a config yaml file
@@ -25,4 +27,5 @@ def subscriber():
     print(request.json, flush=True)
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-app.run()
+if __name__ == "__main__":
+    app.run(port=dapr_port)
