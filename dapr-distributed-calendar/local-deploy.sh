39996a1bace1189007acf7d9538abc5422b65bd1
Matthias Theuermann <mtheuermann@infralovers.com>
2024-01-25 08:44:14 +0100

    fix: added docker installation if necessary

--- dapr-distributed-calendar/local-deploy.sh
+++ dapr-distributed-calendar/local-deploy.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+# Check if Docker is installed
+if command -v docker &>/dev/null; then
+    echo "Docker is already installed."
+else
+    # Install Docker
+    echo "Docker is not installed. Installing..."
+    
+    # Add Docker GPG key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    # Set up the stable Docker repository
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # Update package index
+    sudo apt-get update
+
+    # Install Docker
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+    echo "Docker has been installed."
+fi
+
 # Prepare Dapr environment
 cd $HOME
 mkdir $HOME/.dapr
@@ -12,7 +34,7 @@ dapr init # to initialize dapr
 git clone https://github.com/udhos/update-golang
 cd update-golang
 sudo ./update-golang.sh
-source /etc/profile.d/golang_path.sh
+. /etc/profile.d/golang_path.sh
 
 # Build Golang application
 cd ~/12-factor-app/dapr-distributed-calendar/go
